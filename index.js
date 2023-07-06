b9f46136c19909168233efdd8f5ea3129c3ffd8e
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-06 14:57:19 +0530

    updatded side bar

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-// catch the sidebar
-const menu = document.getElementById('menu')
-const button = document.getElementById('icon')
-const closeIcon = document.getElementById('close-icon')
-
-button.addEventListener('click', function () {
-    console.log(`I was clicked`);
-    let sidebar = menu.style.display;
-    if (sidebar == 'none') {
-        menu.style.display = 'block'
-    } else {
-        menu.style.display = 'none'
-    }
-
-})
-
-closeIcon.addEventListener('click', function () {
-    let sidebar = menu.style.display;
-    if (sidebar == 'none') {
-        menu.style.display = 'block'
-    } else {
-        menu.style.display = 'none'
-    }
-
-})
-
-window.onclick = function (event) {
-    if (event.target == menu) {
-        menu.style.display = "none";
-    }
-} 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
